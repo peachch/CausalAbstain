c222f39e2a99f8807acc8220212bca956c1097bc
yxSun <62700782+peachch@users.noreply.github.com>
2025-05-03 13:46:28 +0800

    Add files via upload

---
+++ causalabstain.py.py
@@ -0,0 +1,285 @@
+import json
+import argparse
+import lm_utils
+import metrics
+import random
+from tqdm import tqdm
+device = "cuda"
+
+if __name__ == "__main__":
+
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-m", "--model", help="which language model to use") # "aya_13b", "chatgpt", "gpt4", llama, phi 
+    argParser.add_argument("-d", "--dataset", help="which dataset") # "mmlu", "hellaswag"
+    argParser.add_argument("-s", "--speak", help="speak which language") # "nl", "es", etc.
+    argParser.add_argument("-o", "--portion", default = 1.0, help="portion of the dataset to use")
+    argParser.add_argument("-l", "--local", default = False, help="local copy of preds saved")
+    argParser.add_argument("-f", "--feedback", default = False, help ="whether to save generated feedbacks in a seperate file")
+    argParser.add_argument("-r", "--related", default = "single", help ="voted method when consider related languages")
+    argParser.add_argument("-n", "--iter_number", type=int, default = 3, help ="iterated number of obtain responses")
+    argParser.add_argument("-t", "--test_or_evaluation",  default = "test", help ="classify save files")
+
+    args = argParser.parse_args()
+    model_name = args.model
+    dataset = args.dataset
+    speak = args.speak
+    approach_type = "self"
+    portion = args.portion
+    local_flag = args.local
+    feedback_flag = args.feedback
+
+    lm_utils.llm_init(model_name)
+
+
+    language_list = ["English", "Russian", "German", "Chinese", "French", "Spanish", "Italian", "Dutch", "Vietnamese",
+                     "Indonesian", "Arabic", "Hungarian", "Romanian", "Danish", "Slovak", "Ukrainian", "Catalan", "Serbian", "Croatian", "Hindi",
+                     "Bengali", "Tamil", "Nepali", "Malayalam", "Marathi", "Telugu", "Kannada", "Deutsch"]
+    
+    # revise the language according to your need
+    language_related_dict = {
+        "zh": ["Chinese", "Chinese", "Chinese", "English", "Russian", "German", "Italian", "Dutch", "Arabic"],
+        "id": ["English","Catalan", "Russian", "Indonesian", "German"],
+        "ar": ["Chinese", "Italian", "Dutch", "Arbic", "English"],
+        "bn": ["Arabic", "Hindi", "Bengali", "Nepali", "Vietanamese", "English", "Telugu", "kannada", "Russian"],
+        "ta": ["Arabic", "Hindi", "Bengali","Chinese", "Italian", "Dutch","Malayalam","Marathi", "Telugu"],
+        "ne": ["Kanaada", "Telugu", "Hindi","Nepali","English"],
+        "te": ["Kannada", "Russian", "Catalan","Telugu","English"],
+        "kn": ["Telugu", "Malaayalam", "Tamil","Kannada","English"]
+    }
+    
+    with open("data/" + dataset + "/" + dataset + "_" + speak + ".json", "r") as f:
+
+        data = json.load(f)
+        data["dev"] = data["dev"][:int(len(data["dev"])*float(portion))]
+        data["test"] = data["test"][:int(len(data["test"])*float(portion))]
+
+        def dict_to_list(data_dict):
+            # 获取字典中任意一个列表的长度
+            list_length = len(next(iter(data_dict.values())))
+            
+            # 创建一个新的列表，长度与字典中的列表长度一致
+            result_list = [[] for _ in range(list_length)]
+            
+            # 遍历字典的每个键和值
+            for key in sorted(data_dict.keys()):
+                for i in range(list_length):
+                    result_list[i].append(data_dict[key][i])
+            
+            return result_list
+        # obtain correct flags
+        N = args.iter_number 
+        n_times_abstain_x = {}
+        n_times_abstain_source = {}
+        n_times_abstain_english = {}
+        answers = []
+        correct_flags = []
+        print("--------get direct answer----------")
+        for d in tqdm(data["test"]):
+            original_prompt = "Question: " + d["question"] + "\n"
+            for key in d["choices"].keys():
+                original_prompt += (key + ": " + d["choices"][key] + "\n")
+            original_prompt += "Choose one answer from the above choices. The answer:"
+            response = lm_utils.llm_response(original_prompt, model_name, probs=False, max_new_tokens=5)
+            print(lm_utils.answer_parsing(response))
+            if lm_utils.answer_parsing(response) == d["answer"]:
+                correct_flags.append(1)
+            else:
+                correct_flags.append(0)
+            # Question -> Answer
+            answers.append(response)
+        print("--------get no fb abstain or not [nde]----------")
+        while N >=1 :    
+            feedback_1 = []
+            feedback_2 = []
+            feedback_3 = []
+            abstain_flags = []
+            abstain_flags_fb1 = []
+            abstain_flags_x = []
+            abstain_scores = []
+            abstain_scores_fb1 = []
+
+            for d, i in tqdm(zip(data["test"], range(len(data["test"])))):
+                original_prompt = "Question: " + d["question"] + "\n"
+                for key in d["choices"].keys():
+                    original_prompt += (key + ": " + d["choices"][key] + "\n")
+                original_prompt += "Choose one answer from the above choices. Proposed answer:"
+
+                response = original_prompt + " " + answers[i].strip() + "\nIs the proposed answer True or False?"
+                response = lm_utils.llm_response(response, model_name, probs=False, max_new_tokens=5)
+
+                parse_answer = lm_utils.answer_parsing(response)
+                if parse_answer == "A":
+                    abstain_flags_x.append(0)
+                elif parse_answer == "B":
+                    abstain_flags_x.append(1)
+                else:
+                    # print("Error: abstain flag not found")
+                    abstain_flags_x.append(random.randint(0, 1))
+
+            n_times_abstain_x[N] = abstain_flags_x       
+            n_times_abstain_english[N] = abstain_flags
+            N -= 1 
+    # print(n_times_abstain_x)
+    # Y(X) 
+    N = args.iter_number
+    n_times_lists_x = dict_to_list(n_times_abstain_x)
+    y_base = [[1/2 for _ in range(2)] for _ in range(len(n_times_lists_x))]    
+    x_distribution = lm_utils.distribution(n_times_lists_x)
+    ndes = lm_utils.JSD(x_distribution, y_base)
+    
+    # casual judge
+    from collections import Counter
+    casual_abstain_flags = []
+    def most_frequesnt_element(input):
+        frequency_count = Counter(input)
+        most_common_element, most_common_count = frequency_count.most_common(2)[0]
+        try:
+            most_common_element_2, most_common_count_2 = frequency_count.most_common(2)[1]
+        except:
+            # when all the element is the same, directly output the first order.
+            return most_common_element, most_common_count, True
+        if most_common_count != most_common_count_2:
+            return most_common_element, most_common_count, True
+        elif most_common_count == most_common_count_2:
+            return most_common_element, most_common_count, False 
+ 
+    assert len(ndes) == len(data["test"])
+    tie_or_nde_flags = []
+    tie_or_nde = []
+    tie_nde_list = []
+    related_feedbacks = []
+    tie_related_maps = []
+    
+    print("--------get feedback and review with feedback [tie]----------")
+    for d, i in tqdm(zip(data["test"], range(len(data["test"])))):
+        if args.related == "two":
+            def calculate_related_language(d, prompt_feedback_related, N, answer):
+                prompt_feedback_experts = prompt_feedback_related
+
+                abstain_flags_dict = {}
+                abstain_scores_dict = {}
+                related_feedback = []
+                while N>= 1:
+                    abstain_flags_fb = []
+                    abstain_scores_fb = []
+                    feedback_related_1 = []
+                    response = lm_utils.llm_response(prompt_feedback_experts, model_name, probs=False, temperature=1)
+                    response = response.split("\n")[0].strip()
+                    if len(response) == 0: # to avoid no generated feedback in multilingual settings
+                        response = "No feedback provided."
+                    feedback_related_1 = response
+                    prompt_area_chair_fb = "Question: " + d["question"] + "\n"
+
+                    for key in d["choices"].keys():
+                        prompt_area_chair_fb += (key + ": " + d["choices"][key] + "\n")
+                    related_feedback.append(feedback_related_1)
+
+                    prompt_area_chair_fb += "Choose one answer from the above choices. Proposed answer: " + answer.strip() + "\n\nFeedback: " + feedback_related_1.strip() + "\n\nBased on the feedback, is the proposed answer True or False? Please respond clearly with 'True' or 'False'."
+                        
+                    response = lm_utils.llm_response(prompt_area_chair_fb, model_name, probs=False, max_new_tokens=10)
+
+                    if lm_utils.answer_parsing(response) == "A":
+                        abstain_flags_fb.append(0)
+                    elif lm_utils.answer_parsing(response) == "B":
+                        abstain_flags_fb.append(1)
+                    else:
+                        # print("Error: abstain flag not found")
+                        abstain_flags_fb.append(random.randint(0, 1))
+
+                    N -= 1
+                    abstain_flags_dict[N] = abstain_flags_fb
+                    abstain_scores_dict[N] = abstain_scores_fb
+                return abstain_flags_dict, abstain_scores_dict, related_feedback
+            
+            original_prompt = "Question: " + d["question"] + "\n"
+            for key in d["choices"].keys():
+                original_prompt += (key + ": " + d["choices"][key] + "\n")
+            original_prompt += "Choose one answer from the above choices. Proposed answer:"
+
+            prompt_feedback = original_prompt + " " + answers[i].strip() + "\nPlease review the proposed answer and provide a paragraph of feedback on its correctness. Feedback should be in <language>.\nFeedback:"
+            prompt_feedback_related = []
+            for k in range(5):
+                specified_language = language_related_dict[speak][k]
+                prompt_feedback_related.append(prompt_feedback.replace("<language>", specified_language))
+            assert len(prompt_feedback_related) == 5 
+            try:
+                abstain_related_flag_1, abstain_related_score_1, related_feedback_1 = calculate_related_language(d, prompt_feedback_related[0], N, answers[i])
+                abstain_related_flag_2, abstain_related_score_2, related_feedback_2 = calculate_related_language(d, prompt_feedback_related[1], N, answers[i])
+                abstain_related_flag_3, abstain_related_score_3, related_feedback_3 = calculate_related_language(d, prompt_feedback_related[2], N, answers[i])
+                related_feedbacks.append([related_feedback_1, related_feedback_2, related_feedback_3])
+
+                # calculate new tie
+                abstain_related_1 = dict_to_list(abstain_related_flag_1)
+                related1_feedback_n_distributions = lm_utils.distribution(abstain_related_1)
+                ties_related1 = lm_utils.JSD(related1_feedback_n_distributions, [x_distribution[i]])
+                
+                abstain_related_2 = dict_to_list(abstain_related_flag_2)
+                related2_feedback_n_distributions = lm_utils.distribution(abstain_related_2)
+                ties_related2 = lm_utils.JSD(related2_feedback_n_distributions, [x_distribution[i]])
+                
+                abstain_related_3 = dict_to_list(abstain_related_flag_3)
+                related3_feedback_n_distributions = lm_utils.distribution(abstain_related_3)
+                ties_related3 = lm_utils.JSD(related3_feedback_n_distributions, [x_distribution[i]])
+                
+                tie_related_abstain_map = {
+                            'tie_related1': [abstain_related_1[0], ties_related1[0]],
+                            'tie_related2': [abstain_related_2[0], ties_related2[0]],
+                            'tie_related3': [abstain_related_3[0], ties_related3[0]],
+                        }
+                this_tie_or_nde_flags_string = ""
+                join_voted = []
+
+                if ndes[i] > ties_related1[0] and ndes[i] > ties_related2[0] and ndes[i] > ties_related3[0]:
+                    join_voted = []
+                else:
+                    join_voted = abstain_related_1[0] + abstain_related_2[0] + abstain_related_3[0]
+                if join_voted == []:
+                    casual_abstain_flags.append(most_frequesnt_element(n_times_lists_x[i])[0])
+                    this_tie_or_nde_flags_string += "ndes"
+                else:
+                    this_tie_or_nde_flags_string += "related"
+                    majorty_voted, count, flag = most_frequesnt_element(join_voted)
+                    if flag:
+                        casual_abstain_flags.append(majorty_voted)
+                    else:
+                        rdm = random.randint(0,1)
+                        casual_abstain_flags.append(rdm)
+                        this_tie_or_nde_flags_string += "& random"
+                tie_or_nde_flags.append(this_tie_or_nde_flags_string)
+                tie_related_maps.append(tie_related_abstain_map) 
+            except Exception as e:
+                print("sth wrong, pass the error")
+                print(e)
+                related_feedbacks.append([])
+                casual_abstain_flags.append(1)
+                this_tie_or_nde_flags_string = "error"
+                tie_related_abstain_map = {
+                            'tie_related1': [],
+                            'tie_related2': [],
+                            'tie_related3': [],
+                        }
+                this_tie_or_nde_flags_string = ""
+                tie_or_nde_flags.append(this_tie_or_nde_flags_string)
+                tie_related_maps.append(tie_related_abstain_map) 
+
+
+    print(casual_abstain_flags)
+
+    casual_abstain_scores = None
+    if local_flag:
+        if args.test_or_evaluation == "test":
+
+            with open("preds_test/" + args.related + str(N) + model_name + "_" + dataset + "_" + speak + "_casual_details.json", "w") as f:
+                json.dump({"ndes":ndes,"n_times_abstain_x":n_times_lists_x, "correct_flags": correct_flags, "casual_abstain_flags": casual_abstain_flags, "casual_abstain_scores": casual_abstain_scores, "tie_ndes_list":tie_nde_list, "tie_or_nde":tie_or_nde, "tie_or_nde_flags":tie_or_nde_flags, "related_fb": related_feedbacks, "tie_related_maps": tie_related_maps}, f, indent=4)
+        else:
+            with open("preds/" +args.related + str(N) + model_name + "_" + dataset + "_" + speak + "_casual_details.json", "w") as f:
+                json.dump({"ndes":ndes,"n_times_abstain_x":n_times_lists_x, "correct_flags": correct_flags, "casual_abstain_flags": casual_abstain_flags, "casual_abstain_scores": casual_abstain_scores, "tie_ndes_list":tie_nde_list, "tie_or_nde":tie_or_nde, "tie_or_nde_flags":tie_or_nde_flags, "related_fb": related_feedbacks, "tie_related_maps": tie_related_maps}, f, indent=4)
+        
+    print("------------------")
+    print("Approach: causal")
+    print("Model:", model_name)
+    print("Dataset:", dataset)
+    print("Language:", speak)
+    print("Type:", approach_type)
+    print(metrics.compute_metrics(correct_flags, casual_abstain_flags, casual_abstain_scores))
+    print("------------------")
---
+++ lm_utils.py
@@ -0,0 +1,276 @@
+import torch
+from ollama import ChatResponse
+from ollama import chat
+import openai
+import os
+import time
+import numpy as np
+import time
+import wikipedia as wp
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
+import math
+from scipy.spatial import distance
+import ollama
+
+device = "cuda"
+def distribution(all_abstains):
+    distributions = []
+    for abstain_flags in all_abstains:
+        counts = {0: 0, 1: 0}
+        for value in abstain_flags:
+            if value in counts:
+                counts[value] += 1
+
+        total_count = sum(counts.values())
+        if total_count == 0:
+            return [0.0, 0.0]
+
+        probabilities = [counts[0] / total_count, counts[1] / total_count]
+
+        # softmax
+        max_prob = max(probabilities)
+        exp_probs = [math.exp(p - max_prob) for p in probabilities] 
+
+        sum_exp_probs = sum(exp_probs) 
+
+        softmax_values = [exp_p / sum_exp_probs for exp_p in exp_probs] 
+        distributions.append(softmax_values)
+
+    return distributions 
+
+def JSD(distribution1, distribution2):
+    jsds = []
+    for i in range(len(distribution1)):
+        p = np.array(distribution1[i])
+        q = np.array(distribution2[i])
+        
+        # 计算平均分布
+        m = 0.5 * (p + q)
+        
+        # 为了避免 log(0) 的情况，使用 np.clip 限制值的范围
+        p = np.clip(p, 1e-10, None)
+        q = np.clip(q, 1e-10, None)
+        m = np.clip(m, 1e-10, None)
+        
+        # 计算 JSD
+        jsd = 0.5 *(np.sum(p* np.log(p / m)) + np.sum(q * np.log(q / m)))
+        jsds.append(jsd)
+    return jsds
+
+def llm_init(model_name):
+    global device
+    global model
+    global tokenizer
+    global pipeline
+
+    if model_name == "aya_13b":
+        device = "cuda"
+        # device = "cpu"
+        tokenizer = AutoTokenizer.from_pretrained("CohereForAI/aya-101")
+        model = AutoModelForSeq2SeqLM.from_pretrained("CohereForAI/aya-101", device_map="auto")
+
+# 定义输入文本
+
+
+    if model_name == "gpt3.5" or model_name == "gpt4"or model_name =="claude":
+        device = "cuda"
+        openai.api_base = "https://openkey.cloud/v1"
+        openai.api_key = "sk-anQGO9X0r1zNkkqcCf29D03eEbDd4e37AbF1Ba986863Da53"
+
+def wipe_model():
+    global device
+    global model
+    global tokenizer
+    global pipeline
+    device = None
+    model = None
+    tokenizer = None
+    pipeline = None
+    del device
+    del model
+    del tokenizer
+    del pipeline
+
+# @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(10))
+def llm_response(prompt, model_name, probs = False, temperature = 0.1, max_new_tokens = 200):
+    if not model_name == "gpt3.5" and not model_name == "gpt4" and not model_name =="claude"and not model_name =="llama" and not model_name=="deepseek"and not model_name=="phi":
+
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        inputs = tokenizer([prompt], return_tensors="pt").to(device)
+        outputs = model.generate(**inputs, max_new_tokens=max_new_tokens, return_dict_in_generate=True, output_scores=True, temperature=temperature, do_sample=True)
+
+
+        transition_scores = model.compute_transition_scores(
+            outputs.sequences, outputs.scores, normalize_logits=True
+        )
+
+        input_length = inputs.input_ids.shape[1]
+        if model_name == "aya_13b" or model_name == "aya23_8b":
+
+            generated_tokens = outputs.sequences[:, 1:-1]
+        else:
+            generated_tokens = outputs.sequences[:, input_length:]
+        generated_text = tokenizer.batch_decode(generated_tokens)[0].strip()
+        token_probs = {}
+        for tok, score in zip(generated_tokens[0], transition_scores[0]):
+            token_probs[tokenizer.decode(tok).strip()] = np.exp(score.item())
+        if probs:
+            return generated_text, token_probs
+        else:
+            return generated_text
+
+    
+    elif model_name == "gpt3.5":
+
+        response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    temperature=temperature,
+                    messages=[
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=max_new_tokens,
+                    logprobs=True,
+                )
+        time.sleep(0.1)
+        token_probs = {}
+
+        for thing in response['choices'][0]['logprobs']["content"]:
+            token_probs[thing["token"]] = np.exp(thing["logprob"])
+        if probs:
+            return response['choices'][0]['message']['content'].strip(), token_probs
+        else:
+            return response['choices'][0]['message']['content'].strip()
+
+    elif model_name == "mistral":
+        response = chat(model='mistral', messages=[
+
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+        ])
+        time.sleep(0.1)
+        token_probs = {}
+        return response['message']['content'].strip()
+    elif model_name == "phi":
+
+        response = chat(model='phi4', messages=[
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+        ])
+        time.sleep(0.1)
+        token_probs = {}
+        return response['message']['content'].strip()
+    elif model_name == "llama":
+        # we use ollama to run llama
+        response = chat(model='llama3.2', messages=[
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+        ])
+        time.sleep(0.1)
+        token_probs = {}
+        return response['message']['content'].strip()
+    elif model_name == "deepseek":
+        response = chat(model='deepseek-r1', messages=[
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+        ])
+        time.sleep(0.1)
+        token_probs = {}
+        return response['message']['content'].strip()
+    elif model_name == "claude":
+
+        response = openai.ChatCompletion.create(
+                    model="claude-3-7-sonnet-20250219",
+                    # model="claude-3-haiku-20240307",
+                    temperature=temperature,
+                    messages=[
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=max_new_tokens,
+                    logprobs=True,
+                )
+        time.sleep(0.1)
+        token_probs = {}
+        if probs:
+            return response['choices'][0]['message']['content'].strip(), token_probs
+        else:
+            return response['choices'][0]['message']['content'].strip()
+    
+    elif model_name == "gpt4":
+        response = openai.ChatCompletion.create(
+                    model="gpt-4o-2024-08-06",
+                    temperature=temperature,
+                    messages=[
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=max_new_tokens,
+                    # logprobs=True,
+                )
+        time.sleep(0.1)
+        token_probs = {}
+        if probs:
+            return response['choices'][0]['message']['content'].strip(), token_probs
+        else:
+            return response['choices'][0]['message']['content'].strip()
+
+def answer_parsing(response):
+    # mode 1: answer directly after
+    temp = response.strip().split(" ")
+    for option in ["A", "B", "C", "D", "E"]:
+        if option in temp[0]:
+            return option
+    # mode 2: "The answer is A/B/C/D/E"
+    temp = response.lower()
+    for option in ["a", "b", "c", "d", "e"]:
+        if "the answer is " + option in temp:
+            return option.upper()
+    # mode 3: "Answer: A/B/C/D/E"
+    temp = response.lower()
+    for option in ["a", "b", "c", "d", "e"]:
+        if "answer: " + option in temp:
+            return option.upper()
+    # mode 4: " A/B/C/D/E " or " A/B/C/D/E."
+    for option in ["A", "B", "C", "D", "E"]:
+        if " " + option + " " in response or " " + option + "." in response:
+            return option
+    # mode 5: "The correct answer is A/B/C/D/E"
+    temp = response.lower()
+    for option in ["a", "b", "c", "d", "e"]:
+        if "the correct answer is " + option in temp:
+            return option.upper()
+    # mode 6: "A: " or "B: " or "C: " or "D: " or "E: "
+    for option in ["A", "B", "C", "D", "E"]:
+        if option + ": " in response:
+            return option
+    # mode 7: "A/B/C/D/E" and EOS
+    try:
+        for option in ["A", "B", "C", "D", "E"]:
+            if option + "\n" in response or response[-1] == option:
+                return option
+    except:
+        pass
+    # mode 8: "true" and "false" instead of "A" and "B" for feedback abstention
+
+    if "true" in response.lower():
+        return "A"
+    if "false" in response.lower():
+        return "B"
+
+    # fail to parse
+    print("--------------fail to parse answer ------------------")
+    return "Z" # so that its absolutely wrong
+
+prompt = "Question: Who is the 44th president of the United States?\nAnswer:"
+
